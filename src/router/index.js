8c94f10197219c162b6b72e656c228cbfbd730fa
12cm <670252095@qq.com>
2017-08-02 16:19:14 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ Vue.use(Router)
 export default new Router({
   mode: 'history', //切换路径模式，变成history模式
   //base: '/mengmeng_follow/dist/',
-  base: '12cm/mengmeng_follow/tree/master/dist/',
+  base: '/12cm/mengmeng_follow/tree/master/dist/',
   scrollBehavior: () => ({ y: 0 }), // 滚动条滚动的行为，不加这个默认就会记忆原来滚动条的位置
   routes: [
     { path: '/page/Login/login', component: login },             //登录
