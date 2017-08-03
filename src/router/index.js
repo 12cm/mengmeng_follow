75648ef25610da6afbe490e06938db843b36a45d
12cm <670252095@qq.com>
2017-08-03 16:28:08 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history', //切换路径模式，变成history模式
-  base: '/mengmeng_follow/dist/',   //npm run dev 时必须注释掉，不然路径会出错
+  //base: '/mengmeng_follow/dist/',   //npm run dev 时必须注释掉，不然路径会出错
   scrollBehavior: () => ({ y: 0 }), // 滚动条滚动的行为，不加这个默认就会记忆原来滚动条的位置
   routes: [
     { path: '/page/Login/login', component: login },             //登录
