7d0e04700fd3ec257cf328873556748dcb4ecd31
12cm <670252095@qq.com>
2017-08-03 11:02:16 +0800

    update

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     //assetsPublicPath: '/mengmeng_follow/dist/',
-    assetsPublicPath: './mengmeng_follow/dist/',
+    assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
